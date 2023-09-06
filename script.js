46d67d54a3f20feba34d5789c169ca33528d7d78
Jordan Perkins <jordanperkins@hotmail.co.uk>
2023-09-06 20:04:52 +0100

    initial commit GUI

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ function game(){
     let playerScore = 0
     let computerScore = 0
     
-    for (let i = 1; i <= 5; i++){
+    
         let computerSelection = getComputerChoice();
         let playerSelection = prompt("Rock, Paper Or Scissors?")
-        console.log(i)
+        
         console.log(playRound(playerSelection,computerSelection))
         if (playRound(playerSelection,computerSelection).includes("Win")){
             playerScore++
@@ -57,9 +57,8 @@ function game(){
         else if (playRound(playerSelection,computerSelection).includes("Lose")){
             computerScore++
         }
-        const getRoundPlural = (i==1) ? "round" : "rounds"
-        console.log(`The score after ${i} ${getRoundPlural} is Player ${playerScore} - Computer ${computerScore}`)
-    }
+
+        console.log(`The score is Player ${playerScore} - Computer ${computerScore}`)
+    
 }
 
-game()
